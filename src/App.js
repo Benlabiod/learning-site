8941729014ebd3302a1ba2e382fd5e3de523b22f
lotfi_benlabiod <benlabiod.lotfi.1@gmail.com>
2021-08-29 13:55:54 +0200

    modif 1 to publish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-center items-center bg-teal-400 min-h-screen">
+        <h2 className="text-orange-600 text-6xl"> Hello orange text</h2>
+      </div>
     </div>
   );
 }
